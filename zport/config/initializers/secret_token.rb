4e9047cb3d1b5e37b58b848cba0dbba4c6f0b33f
Oleksiy Zelenyuk <ozelenyuk@amplify.com>
2014-01-21 22:53:16 +0200

    initial commit

---
+++ zport/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zport::Application.config.secret_key_base = 'd9cee0e6ea404e822a87db0cfb9f2a21a9cf2ef8e7ffff29eb397d7cb766251a253ff3a35c6581de6508f00334c862197c298f71c618c7ab0f353e2a3f1a8b06'
