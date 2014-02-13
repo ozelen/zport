9755dc5c1add12dc48daf27752e91f4548d0644c
Oleksiy Zelenyuk <ozelenyuk@amplify.com>
2014-02-13 09:24:37 +0200

    homepage route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Zport::Application.routes.draw do
   resources :pages
   root 'pages#home'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
