daa80197622800145a7f9e8fb298dea0882737d4
Oleksiy Zelenyuk <ozelenyuk@amplify.com>
2014-09-16 21:34:01 +0300

    add fonts to precompilation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,11 @@ Zport::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile << Proc.new { |path|
+    if path =~ /\.(eot|svg|ttf|woff)\z/
+      true
+    end
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
