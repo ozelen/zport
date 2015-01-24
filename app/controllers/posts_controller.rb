0bd5a193313747705ae76025ffbad80f2a6b94cb
Oleksiy Zelenyuk <ozelenyuk@amplify.com>
2015-01-24 10:56:50 +0200

    open blog

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :authorize
+  before_action :authorize, except: [:index, :show]
   before_action :tag_cloud
 
   # GET /posts
