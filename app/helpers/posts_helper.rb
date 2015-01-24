1130b658916ab7b872c6499b3f9e4072004ab500
Oleksiy Zelenyuk <ozelenyuk@amplify.com>
2015-01-24 12:01:25 +0200

    tag links for posts

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,3 +1,7 @@
 module PostsHelper
   include ActsAsTaggableOn::TagsHelper
+
+  def tag_links post
+    post.tags.map { |tag| link_to tag.name, posts_by_tag_path(tag.name) }.join(', ').html_safe
+  end
 end
